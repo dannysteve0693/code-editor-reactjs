8279690d69e419fe9840dcced3e85ecfbb019e90
Danny <dannysteve0693@gmail.com>
2025-11-07 14:36:32 +0700

    Added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import { Box, Center } from "@chakra-ui/react";
+
+
+
+const Footer = () => {
+    return (
+        <Center>
+            <Box mt={3}>
+                <p>
+                    Created with ❤️ by <a href="https://dannyhive.my.id" className="text-blue-400 hover:underline">Danny</a>© {new Date().getFullYear()} | Made for developers, free for everyone. | If you like this project, consider sharing or supporting it 🙌
+                </p>
+            </Box>
+
+        </Center>
+    );
+};
+
+export default Footer;
